8fa59e1083081fc61fdf8b27c491ae40f9f67660
RushikeshMore11 <morerushi56@gmail.com>
2022-09-23 13:05:44 +0530

    Marco Polo added

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
-  
+  const [alter,setAlter] = useState(false);
+
+  const handleClick = () =>{
+    if(alter == false)
+    setAlter(true);
+    else
+    setAlter(false);
+  }
+
   return (
     <div id="main">
-
+      <h1 id="marco-polo">{alter?"Marco":"Polo"}</h1>
+      <button id="marco-polo-toggler" onClick={handleClick}>{alter?"Polo":"Marco"}</button>
     </div>
   )
 }
