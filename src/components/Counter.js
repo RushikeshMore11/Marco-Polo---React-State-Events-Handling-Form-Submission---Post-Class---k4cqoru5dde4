c1f99af6a2c26a247a8ac457140fc7e9998de247
rushikesh-scalex <rushikeshm@scalextechnologies.com>
2022-12-12 16:01:53 +0530

    css to reset applied

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { decreaseValue, increaseValue, resetValue } from '../redux/Actions';
+import "../styles/App.css";
 
 const Counter = () => {
     const {count} = useSelector(state=>state.counterReducer);
@@ -11,7 +12,7 @@ const Counter = () => {
         <>
         <div className='counterHolder'>
             <p><button className='cntBtn' onClick={()=>{dispatch(increaseValue())}}>INCREMENT</button></p>
-            <p className='actualCount'>Count is {count}</p>
+            <p className='actualCount'>Count is <span id='count'>{count}</span></p>
             <div className='resetHolder'>
             <p><button className='cntBtn' onClick={()=>{dispatch(decreaseValue())}}>DECREMENT</button></p>
             <p>
